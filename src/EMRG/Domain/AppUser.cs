8ecd5e2de5bced33c0210f3ea7ae8d056ee2993c
Ashiful Arefin <arefin.ashiful@gmail.com>
2019-03-29 20:43:32 +0600

    Department Admin  Login

--- src/EMRG/Domain/AppUser.cs
+++ src/EMRG/Domain/AppUser.cs
@@ -4,6 +4,8 @@ namespace Domain
 {
     public class AppUser : IdentityUser
     {
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
         public Role Role { get; set; }
     }
 }
---
+++ src/EMRG/Web/Areas/DepartmentAdmin/Pages/Register.cshtml.cs
@@ -0,0 +1,129 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Data.Core;
+using Domain;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Areas.DepartmentAdmin.Pages
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+        private readonly IUnitOfWork _db;
+
+        public RegisterModel(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender,
+            IUnitOfWork db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+            _db = db;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public class InputModel
+        {
+
+            [Required]
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
+
+            [Required]
+            public Role Role { get; set; } = Role.DepartmentAdmin;
+
+            [Required]
+            [Display(Name = "Department")]
+            public int DepartmentId { get;set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        public async Task OnGetAsync(string returnUrl = null) {
+            ViewData["Departments"] = new SelectList(
+                await _db.Departments.GetAll(), 
+                nameof(Department.Id), 
+                nameof(Department.Code)
+            );
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    Role = Input.Role,
+                    UserName = Input.UserName,
+                    Email = Input.Email,
+                    DepartmentId = Input.DepartmentId
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    string code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    string callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { userId = user.Id, code = code },
+                        protocol: Request.Scheme);
+
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return LocalRedirect(returnUrl);
+                }
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}
